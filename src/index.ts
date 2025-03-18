72aeb042f5d8f926c1ce3b489f6b1c9ea15f09da
Anthony Astige <Anthony@Astige.com>
2025-03-18 18:03:23 -0500

    No configs needed ... simplify

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,3 @@ import { noImportAsRule } from "./rules/no-import-as";
 export const rules = {
   "no-import-as": noImportAsRule,
 };
-
-export const configs = {
-  recommended: {
-    rules: {
-      "eslint-plugin-no-named-import-alias/no-import-as": "error",
-    },
-  },
-};
